a4dcf2d52e2bf6d67865b76ece7471d05dac8510
TheWireMonkey <thewiremonkey@gmail.com>
2020-09-01 21:26:07 -0400

    update with comments

--- Penguin Report Generator.R
+++ Penguin Report Generator.R
@@ -12,33 +12,39 @@ pdata <- palmerpenguins::penguins_raw %>%
     replacement = "\n("
   ))
 
-#create a list for mapping
+#create a list of study names over which we'll map the function
 studies <- unique(pdata$study_name)
 
 generate_reports <- function(study) {
   #I like to superassign some objects for troubleshooting, but it isn't necessary
   study <<- study
-  p_df <<- pdata %>% filter(study_name == study)
+  #I used the below in my talk, but have changed to passing the data via the params argument in the rmarkdown::render() function
+  # p_df <<- pdata %>% filter(study_name == study)
   min_date <- min(p_df$date_egg, na.rm = TRUE)
   max_date <- max(p_df$date_egg, na.rm = TRUE)
   title <- paste("Study: ", study)
   subtitle <- paste("From: ", min_date, "To: ", max_date)
 
   rmarkdown::render(
-    input = "penguin report.Rmd",
     #The RMarkdown file that will produce the report
-    output_format = "word_document",
+    input = "penguin report.Rmd",
     #How should the report be produced. Can be a single value or a vector
-    output_dir = "./reports",
+    output_format = "word_document",
     #Directory to write reports to if not the working directory
+    output_dir = "./reports",
     params = list(
-      #Values to pass to the frontmatter.
+      #Values to pass to the YAML header or to use as a variable within the .rmd file
       Date = Sys.Date(),
       Title = title,
       Subtitle = subtitle,
-      Study = study
+      Study = study,
+      Min_Date = min_date,
+      Max_Date = max_date,
+      Data = pdata %>% filter(study_name == study) #you can pass the data as a parameter!
     ),
     output_file = paste0("Report_", study) #Give each file a unique name
   )
 }
-map(studies, ~ generate_reports(.x))
+
+#run the function on each element of the list of Palmer penguin studies
+purrr::map(studies, ~ generate_reports(.x))
